7a379a275edc21407b9a4356b4913cf139691ef9
Logan March <logmarch2@aol.com>
2024-12-06 19:54:27 -0500

    Initial Commit

---
+++ FindPugie/FindPugieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindPugieApp.swift
+//  FindPugie
+//
+//  Created by Logan March on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FindPugieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FindPugieTests/FindPugieTests.swift
@@ -0,0 +1,17 @@
+//
+//  FindPugieTests.swift
+//  FindPugieTests
+//
+//  Created by Logan March on 12/6/24.
+//
+
+import Testing
+@testable import FindPugie
+
+struct FindPugieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
